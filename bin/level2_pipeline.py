d7f4d49b8f28a112c1a20e6812e9d810d2562e45
James Chiang <jchiang87@gmail.com>
2016-12-20 14:26:22 -0800

    update import and function calls to use latest interface

--- bin/level2_pipeline.py
+++ bin/level2_pipeline.py
@@ -6,14 +6,14 @@ $ setup -m none -r and_files astrometry_net_data
 have been run and which contains an images/ and and_files/
 subdirectories.
 """
-from desc.level2_pipeline import ingestImages, getVisits, Level2_Pipeline
+from desc.level2_pipeline import ingest_images, get_visits, Level2_Pipeline
 
 phosim_dir = 'images'
 image_repo = 'image_repo'
 output_repo = 'output_repo'
 
-ingestImages(phosim_dir, image_repo)
-visits = getVisits(image_repo)
+ingest_images(phosim_dir, image_repo)
+visits = get_visits(image_repo)
 l2 = Level2_Pipeline(image_repo, output_repo, visits)
 l2.run(dry_run=False)
 l2.report_failures()
